c534b8f625e51ce75fd15a14b0717e30146fd8b3
aidan BL <aidanjohn13@gmail.com>
2024-01-16 12:37:16 -0600

    added README file

--- assets/script.js
+++ assets/script.js
@@ -15,3 +15,30 @@ if (topic === "HTML"){
 for (x = 0; x < topics.length; x++){
     console.log(topics[x]);
 }
+var randomTopic = topics[Math.floor(Math.random() * topics.length)]
+
+function listTopics(){
+    for (x = 0; x < topics.length; x++){
+        console.log(topics[x]);
+    }
+}
+
+function selectTopic(){
+    if (randomTopic === "HTML"){
+        console.log("Let's study HTML!");
+    } else if (randomTopic === "CSS") {
+        console.log("Let's study CSS!");
+    } else if (randomTopic === "Git"){
+        console.log("Let's study Git!");
+    } else if (randomTopic === "JavaScript"){
+        console.log("Let's study JavaScript");
+    } else {
+        console.log("Please try again!");
+    }
+}
+
+console.log("Here are the topics I learned during prework: ");
+listTopics();
+console.log("Which topic should I study first?");
+selectTopic();
+
